2d831797f9d8c433a723a35f5de4606fa1e9ee4f
utkarsh3105 <trivediutkarsh31@gmail.com>
2023-09-22 04:20:26 +0530

    Third

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import TextSection from "./Components/TextSection";
-import React from "react";
+import React,{useState} from "react";
 
 function App() {
+  const [mode, setmode] = useState('light');
+
+  const toggleMode=()=>
+  {
+    if(mode==='light')
+    {
+      setmode('dark');
+    }
+    else
+    {
+      setmode('light');
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils"  mode={mode} toggleMode={toggleMode}/>
       <h1 className="text-center my-3">Enter the text to analyze</h1>
       <TextSection />
     </>
